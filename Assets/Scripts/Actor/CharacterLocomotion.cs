25c14a788c943e686505a8cd64442857a7744e66
CesarVinken <39000090+CesarVinken@users.noreply.github.com>
2019-09-30 22:44:20 +0200

    Player moves to mouse click position

---
+++ Assets/Scripts/Actor/CharacterLocomotion.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class CharacterLocomotion : MonoBehaviour
+{
+    private Vector2 _target;
+
+    public float BaseSpeed = 8f;
+    public float Speed;
+
+    public void Awake()
+    {
+        Speed = BaseSpeed;
+    }
+
+    void Update()
+    {
+        CheckMouseInput();
+        if(PlayerCharacter.Instance.CharacterActionState == CharacterActionState.Moving)
+        {
+            HandleMovement();
+        }
+    }
+
+    private void CheckMouseInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            SetLocomotionTarget(_target);
+        }
+    }
+
+    private void SetLocomotionTarget(Vector3 target)
+    {
+        Debug.Log("New location target set for player: " + target);
+
+        // Change animation
+
+        PlayerCharacter.Instance.SetCharacterActionState(CharacterActionState.Moving);
+    }
+
+    private void HandleMovement()
+    {
+        if(transform.position.x != _target.x || transform.position.y != _target.y)
+        {
+            transform.position = Vector2.Lerp(transform.position, _target, Speed * Time.deltaTime);
+            if (Vector2.Distance(transform.position, _target) < 0.01)
+            {
+                PlayerCharacter.Instance.SetCharacterActionState(CharacterActionState.Idle);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Actor/PlayerCharacter.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour
+{
+    public static PlayerCharacter Instance;
+
+    public Animator Animator;
+
+    public CharacterLocomotion PlayerLocomotion;
+    public CharacterActionState CharacterActionState;
+    void Awake()
+    {
+        //if (Animator == null)
+        //    Animator = GetComponent<Animator>();
+        Instance = this;
+
+        PlayerLocomotion = gameObject.AddComponent<CharacterLocomotion>();
+        SetCharacterActionState(CharacterActionState.Idle);
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    public void SetCharacterActionState(CharacterActionState newState)
+    {
+        Debug.Log($"CharacterActionState set to {newState}");
+        CharacterActionState = newState;
+    }
+}
